e0abbfe37239c97f547346058fc46aa1b6c1760b
Daniele Solombrino <daniele.solombrino@gmail.com>
2021-05-25 19:43:20 +0200

    Delete export_virtual_env.sh

--- utils/export_virtual_env.sh
+++
@@ -1,8 +0,0 @@
-mkdir sr
-cd sr
-python3 -m virtualenv venv
-source venv/bin/activate
-pip3 install speechrecognition
-deactivate
-cd venv/lib/python3.9/site-packages
-zip -r ~/Desktop/sr/sr.zip speech_recognition
