8ef094f9775499183629fcc23cd24c14457e1854
Daniele Solombrino <daniele.solombrino@gmail.com>
2021-06-20 20:03:33 +0200

    Update compute_min_max_avg.sh
    Fix logic error for min, max and avg execution times of repeated runs

--- utils/compute_min_max_avg.sh
+++ utils/compute_min_max_avg.sh
@@ -4,8 +4,8 @@ args=("$@")
 
 file_name="${args[0]}"
 
-min=`awk 'BEGIN{a=999999999999}{if ($1<0+a) a=$1} END{print a}' $file_name`
-max=`awk 'BEGIN{a=           0}{if ($1>0+a) a=$1} END{print a}' $file_name`
-avg=`awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }' $file_name`
+repeated_min=`awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }' $file_name`
+repeated_max=`awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }' $file_name`
+repeated_avg=`awk '{ sum += $3; n++ } END { if (n > 0) print sum / n; }' $file_name`
 
-echo "${min}, ${max}, ${avg}"
+echo "${repeated_min}, ${repeated_max}, ${repeated_avg}"
