2b6eaf59a11a7ce92f659fe351a39ada48daba5b
Daniele Solombrino <daniele.solombrino@gmail.com>
2021-06-21 11:39:11 +0200

    Update speech2text.py
    Code refactoring

--- src/aws/lambdas/speech2text.py
+++ src/aws/lambdas/speech2text.py
@@ -3,51 +3,53 @@ import urllib.parse
 import boto3
 
 import sys
+
 import speech_recognition as sr
 
 s3 = boto3.client('s3')
 
-def lambda_handler(event, context):
 
-    #bucket = event['Records'][0]['s3']['bucket']['name']
-    bucket = event['name']
-    key = urllib.parse.unquote_plus(
-        #event['Records'][0]['s3']['object']['key'], encoding='utf-8'
-        event['key'], encoding='utf-8'
-    )
+def lambda_handler(event, conrecognized_text):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
     try:
-        ### --- REMEMBER TO CHANGE BUCKET NAME and FILE --- ###
-        response = s3.get_object(Bucket='nlpipe-test-stt', Key='a.wav')
-        ### --- REMEMBER TO CHANGE BUCKET NAME and FILE --- ###
-
-        #print("[DEBUG] CONTENT TYPE: " + str(response.keys()))
-        #print("[DEBUG] BODY: " + str(response['Body']))
-
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("[DEBUG] Content type: " + response['ContentType'] + "\n\n")
+        print("[DEBUG] Bucket      : " + bucket + "\n\n")
+        print("[DEBUG] file_name    : " + key + "\n\n")
+        #return response['ContentType']
     except Exception as e:
         print(e)
-        print(
-            'Error getting object {} from bucket {}' +
-            'Make sure they exist and your bucket is in the same region as this function.'
-            .format(key, bucket)
-        )
+        print('\n\nError getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.\n\n'.format(key, bucket))
         raise e
+
+    print('[DEBUG] Getting a sound recognizer instance\n\n')
 
     recognizer = sr.Recognizer()
 
+    print('[DEBUG] Got     a sound recognizer instance\n\n')
 
     with sr.AudioFile(response['Body']) as source:
-        print('[DEBUG] Opened file')
+        print('[DEBUG] Opened file ' + key + " from bucket " + bucket + "\n\n")
         recorded_audio = recognizer.listen(source)
 
     try:
-        text = recognizer.recognize_google(recorded_audio, language="en-US")
-        print('[DEBUG] recognized text: ' + text)
+        recognized_text = recognizer.recognize_google(recorded_audio, language="en-US")
+        print('[DEBUG] Recognized text: ' + recognized_text + "\n\n")
 
     except Exception as ex:
         print(ex)
 
+
+    #return response['ContentType']
+    #return recognized_text
     return {
-        'statusCode': 200,
-        'body': json.dumps('Success!')
-    }
+            'statusCode': 200,
+            'body': json.dumps(recognized_text),
+            'file_name': key,
+            'recognized_text': recognized_text
+        }
