39e2282fd60bc8f5f32d901547b7d7ce3f65570d
Daniele Solombrino <daniele.solombrino@gmail.com>
2021-06-21 17:34:05 +0200

    Update speech2text_invoke_batch.sh

--- utils/speech2text_invoke_batch.sh
+++ utils/speech2text_invoke_batch.sh
@@ -16,7 +16,7 @@ do
   --function-name speech2text \
   --invocation-type Event \
   --payload "fileb://../deploy/Lambda/speech2text/inputs/${file_name}/${file_name}_${i}.json" \
-  ../deploy/Lambda/speech2text/outputs/speech2text_output.json > \
+  ../deploy/Lambda/speech2text/outputs/speech2text_output.json >> \
   ../deploy/Lambda/speech2text/outputs/speech2text_output.txt &
 
 done
