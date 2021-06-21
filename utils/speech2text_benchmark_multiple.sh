9d5b24df56294e17fa65c6adc4458ff2881398f6
Daniele Solombrino <daniele.solombrino@gmail.com>
2021-06-21 20:34:26 +0200

    Update speech2text_benchmark_multiple.sh

--- utils/speech2text_benchmark_multiple.sh
+++ utils/speech2text_benchmark_multiple.sh
@@ -24,7 +24,8 @@ echo "Batch size: $batch_size"
 for (( i=1; i<=$num_of_batches; i++ ))
 do
 
-  bash speech2text_benchmark_batch.sh $batch_size 0 $full_file_name $num_of_batches
+  bash speech2text_benchmark_batch.sh $batch_size 0 $full_file_name \
+  $num_of_batches
 
   if [ "$bool_verbose" -eq "1" ]; then
     echo "Batch $i/$num_of_batches DONE!"
@@ -35,11 +36,15 @@ done
 echo ""
 echo ""
 
-repeated_min=`awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }' $output_file_name`
-repeated_max=`awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }' $output_file_name`
-repeated_avg=`awk '{ sum += $3; n++ } END { if (n > 0) print sum / n; }' $output_file_name`
+repeated_min=`awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }' \
+$output_file_name`
+repeated_max=`awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }' \
+$output_file_name`
+repeated_avg=`awk '{ sum += $3; n++ } END { if (n > 0) print sum / n; }' \
+$output_file_name`
 
-echo "Updated min, max and avg: ${repeated_min}, ${repeated_max}, ${repeated_avg}"
+echo "Updated min, max and avg: \
+${repeated_min}, ${repeated_max}, ${repeated_avg}"
 
 # waiting for all the """parallel""" speech2text to complete
 # wait
