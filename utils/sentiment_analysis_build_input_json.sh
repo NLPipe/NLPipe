4ecc11bd658e7ae9b39ad2db98136020fde43ac1
Daniele Solombrino <daniele.solombrino@gmail.com>
2021-06-22 18:04:50 +0200

    Create sentiment_analysis_build_input_json.sh

---
+++ utils/sentiment_analysis_build_input_json.sh
@@ -0,0 +1,45 @@
+args=("$@")
+
+# parsing args from command line
+sentences_max_num=${args[0]}
+
+if [[ -z "${sentences_max_num// }" ]]; then
+  sentences_max_num=1
+fi
+
+# echo $sentences_max_num
+
+echo "Removing old files in current ../deploy/Lambda/sentiment_analysis/inputs directory..."
+rm -f ../deploy/Lambda/sentiment_analysis/inputs/*.json
+echo "Old files removal DONE!"
+
+echo "Creating input JSON files..."
+
+i=1
+for file_name in ../sentences/*.txt; do
+  echo "JSON $i / $sentences_max_num populated w/ sentences in file $file_name"
+
+  file_content=$(<$file_name)
+
+  # echo $file_content
+
+  json="{
+    \"responsePayload\": {
+      \"statusCode\": 200,
+      \"body\": \"${file_content}\",
+      \"file_name\": \"${i}\",
+      \"recognized_text\": \"${file_content}\"
+    }
+  }"
+
+  echo $json >> "../deploy/Lambda/sentiment_analysis/inputs/${i}.json"
+
+
+  ((i=i+1))
+
+  if (( $i > $sentences_max_num )); then
+    break
+  fi
+done
+
+echo "JSON files creation DONE!"
